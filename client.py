2e9f74901f2d1bed7c51b2faefd48b34887bd6a6
Prashansha2005 <prashansharawat36@gmail.com>
2026-06-30 14:34:09 +0530

    Delete client.py

--- client.py
+++
@@ -1,6 +0,0 @@
-import google.generativeai as genai
-
-genai.configure(api_key="AIzaSyClmugZy06i0WS4PIpzw_P2NzIupFu53_s")
-model = genai.GenerativeModel(model_name="gemini-1.5-flash") 
-response = model.generate_content("do you love me")
-print(response.text)
